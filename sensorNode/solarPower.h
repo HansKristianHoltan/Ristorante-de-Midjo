1d9b6e4578c0e7fceb585a2264f927db66130c6e
proxy2k13 <127874191+proxy2k13@users.noreply.github.com>
2023-05-03 10:07:33 +0200

    Update solarPower.h

--- sensorNode/solarPower.h
+++ sensorNode/solarPower.h
@@ -43,69 +43,8 @@ solarChargeSpeed = 1000/sunValue; //Regner ut en ladehastighet som gjør at mye 
    }
     }
     }
-    if(powerBankBattery <= 150){
-               if(millis()- lowBatError >= errLEDTime){  // blinker det røde lyset lfor å symbolisere lavt batterinivå på ladestasjonen
-          lowBatError = millis(); 
-          
-          if(rLEDstate == off){        
-          digitalWrite(rLED, HIGH); // Slår av lavt nivå varslingslys
-          rLEDstate = on;
-          }
-          else if (rLEDstate == on){
-            digitalWrite(rLED, LOW); // Slår av lavt nivå varslingslys
-          rLEDstate = off; 
-          }                           
-        }
-    }
-    else{
-      digitalWrite(rLED, LOW);
-    }
+    
+lightbarpBB (); // kjører funksjonen som illustrerer ladenivå
 
-     switch (powerBankBattery){ //Visualisering av powerBank batterinivå om Zumobilen ikke lader     
-      case 0 ... 100: // 0 til 100 (0-20%) av brukelig nivå 
-        digitalWrite(LED1, HIGH); // LED1 på
-        digitalWrite(LED2, LOW);
-        digitalWrite(LED3, LOW);
-        digitalWrite(LED4, LOW);
-        digitalWrite(LED5, LOW);
-       
-      break;
-
-      case 101 ... 200: // 101 til 200 (21-40%) av brukelig nivå 
-        digitalWrite(LED1, HIGH); // LED1 på
-        digitalWrite(LED2, HIGH); // LED2 på
-        digitalWrite(LED3, LOW);
-        digitalWrite(LED4, LOW);
-        digitalWrite(LED5, LOW);
-              
-      break;
-
-      case 201 ... 300: // 201 til 300 (41-60%) av brukelig nivå 
-        digitalWrite(LED1, HIGH); // LED1 på
-        digitalWrite(LED2, HIGH); // LED2 på
-        digitalWrite(LED3, HIGH); // LED3 på
-        digitalWrite(LED4, LOW);
-        digitalWrite(LED5, LOW);
-
-      break;
-
-      case 301 ... 400: // 301 til 400 (61-80%) av brukelig nivå 
-        digitalWrite(LED1, HIGH); // LED1 på
-        digitalWrite(LED2, HIGH); // LED2 på
-        digitalWrite(LED3, HIGH); // LED3 på
-        digitalWrite(LED4, HIGH); // LED4 på
-        digitalWrite(LED5, LOW);
-        
-      break;
-
-      case 401 ... 500: // 401 til 500 (81-100%) av brukelig nivå    
-        digitalWrite(LED1, HIGH); // LED1 på
-        digitalWrite(LED2, HIGH); // LED2 på
-        digitalWrite(LED3, HIGH); // LED3 på
-        digitalWrite(LED4, HIGH); // LED4 på
-        digitalWrite(LED5, HIGH); // LED5 på
-        
-      break;   
-  }
  }
 }
