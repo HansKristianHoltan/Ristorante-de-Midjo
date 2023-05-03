915ed5c52c0f24142df384a2376bc2d13642d525
proxy2k13 <127874191+proxy2k13@users.noreply.github.com>
2023-05-03 10:05:31 +0200

    Update batteryCharge.h

--- sensorNode/batteryCharge.h
+++ sensorNode/batteryCharge.h
@@ -2,15 +2,28 @@ void chargeZumo(){
    
 if (isCharging) {
   digitalWrite(rLED, HIGH); //setter rLED high for å symbilsere at lading av ZumoBil pågår
+
+  //
   if (abortCharge == true && (newBatteryPer >= abortLevel)){ //checks if abortCharging is active, and if new battery% is above the abortlevel.
      
         Serial.println("CHARGING ABORTED");
-        doneCharging(); //running the function isDoneCharging that resets charging values and sends the new data to the server.    
-        }
+        doneCharging(); // funksjonen resetter alle verdier, og avslutter ladingen, og publiserer data til mqtt serveren    
+    }
 
    if (newBatteryPer >= chargeStop){
-    doneCharging(); // running the function isDoneCharging that resets charging values and sends the new data to the server.      
+    doneCharging(); // funksjonen resetter alle verdier, og avslutter ladingen, og publiserer data til mqtt serveren        
         }
+    if(powerBankBattery <= pb_threshold){ // sjekker om nivået på powerBank er under minste nivå
+      if(sunValue >= sunThreshold){ // sjekker om  
+          batteryCharge = false; // tillater lading med solcelle om det er nok sol
+      }
+        
+      else{ // stopper lading om powerBank batteriet kommer under minste nivå, og solnivået er for svakt
+        Serial.println("PowerBankBattery is LOW.. Charging stopped at: ");
+        Serial.print(newBatteryPer);
+        doneCharging(); // funksjonen resetter alle verdier, og avslutter ladingen, og publiserer data til mqtt serveren 
+      }      
+    }
     
   if((millis() - zumoChargeMillis >= (chargeSpeed)) && batteryCharge == false &&  (sunValue >= sunThreshold)){
       switch (chargePer){
@@ -60,66 +73,8 @@ if (isCharging) {
       }
    }  
 
-// switch case for å blinke det intervallet (1/5, 2/5, 3/5, 4/5 ,5/5) som lades opp    
-if(millis() - blinkTimer >= blinkInterval){
-      switch (newBatteryPer) {  
-       case 0 ... 20:
-    
-            blinkTimer = millis();
 
-            digitalWrite(LED1, !digitalRead(LED1)); // blinker LED1 ved å sette motsatt verdi hver gang case statement kjører
-            digitalWrite(LED2, LOW);
-            digitalWrite(LED3, LOW);
-            digitalWrite(LED4, LOW);
-            digitalWrite(LED5, LOW);
-            
-       break;
-
-       case 21 ... 40:
-                
-            blinkTimer = millis();
-            digitalWrite(LED1, HIGH);
-            digitalWrite(LED2, (!digitalRead(LED2))); // blinker LED2 ved å sette motsatt verdi hver gang case statement kjører
-            digitalWrite(LED3, LOW);
-            digitalWrite(LED4, LOW);
-            digitalWrite(LED5, LOW);
-
-       break;
-
-       case 41 ... 60:
-
-            blinkTimer = millis();
-            digitalWrite(LED1, HIGH);
-            digitalWrite(LED2, HIGH);
-            digitalWrite(LED3, (!digitalRead(LED3)));  // blinker LED3 ved å sette motsatt verdi hver gang case statement kjører
-            digitalWrite(LED4, LOW);
-            digitalWrite(LED5, LOW);
-            
-       break;
-
-       case 61 ... 80:
-
-            blinkTimer = millis();
-            digitalWrite(LED1, HIGH);
-            digitalWrite(LED2, HIGH);
-            digitalWrite(LED3, HIGH);
-            digitalWrite(LED4, (!digitalRead(LED4)));  // blinker LED4 ved å sette motsatt verdi hver gang case statement kjører
-            digitalWrite(LED5, LOW);           
-
-
-       break;
-
-       case 81 ... 100:
-
-            blinkTimer = millis();
-            digitalWrite(LED1, HIGH);
-            digitalWrite(LED2, HIGH);
-            digitalWrite(LED3, HIGH);
-            digitalWrite(LED4, HIGH);
-            digitalWrite(LED5, (!digitalRead(LED5)));  // blinker LED5 ved å sette motsatt verdi hver gang case statement kjører
-
-       break;    
- }            
-}
+   
+lightBarZumo();
 }
 }
